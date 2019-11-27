733387c0fb508f41e8bd1a02e77abd0cabd9466d
caparrosmatias <55497544+caparrosmatias@users.noreply.github.com>
2019-11-27 13:19:22 -0300

    Add files via upload

---
+++ web/index3.php
@@ -0,0 +1,64 @@
+<html><head><title>Estacion Meteorologica</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+        <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+        <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="index3.css" rel="stylesheet" type="text/css">
+    </head><body>
+
+<div class="section"><div class="container"><div class="row"><div class="col-md-12"><div class="page-header"><h1 class="text-center">Estación Meteorológica</h1><br><br><h2 class="text-center">Visualización de datos</h2></div></div></div></div></div>
+<h4 class="text-center">
+<?php
+$conexion=mysqli_connect("mysql.hostinger.com.ar","u196236622_meteo","mati9603","u196236622_meteo") or
+    die("Problemas con la conexión");
+
+//$rs = mysql_query("SELECT MAX(fecha, id, templm, tempdht, tempprom, presion, humedadamb, humedadsuelo1, humedadsuelo2, humedadsuelo3, luz, procio, velocidadviento, stermica ) AS numero FROM  `estacion`");
+
+$numero=mysqli_query($conexion,"SELECT * FROM estacion ORDER BY numero DESC LIMIT 1 , 1") or
+  die("Problemas en el select:".mysqli_error($conexion));
+
+//$extraido= mysqli_fetch_array($numero);
+/*
+$maximo= $extraido['numero'];
+
+$datos=mysqli_query($conexion,"SELECT * FROM  `estacion` WHERE  `numero` ="$maximo" LIMIT 0 , 30") or
+  die("Problemas en el select:".mysqli_error($conexion));
+
+$informacion= mysqli_fetch_array($fecha);
+
+echo $informacion['fecha'] */
+
+
+while ($extraido=mysqli_fetch_array($numero))
+{
+  echo "<b>Fecha: </b>".$extraido['fecha']."<br>";
+  echo "<b>Numero de registro: </b>".$extraido['numero']."<br>";
+  echo "<b>Identificador de estacion: </b>".$extraido['id']."<br>";
+  echo "<b>Temperatura LM 35: </b>".$extraido['templm']." grados Celsius<br>";
+  echo "<b>Temperatura DHT 11: </b>".$extraido['tempdht']." grados Celsius<br>";
+  echo "<b>Temperatura BMP 180: </b>".$extraido['tempbmp']." grados Celsius<br>";
+  echo "<b>Temperatura PROMEDIO: </b>".$extraido['tempprom']." grados Celsius<br>";
+  echo "<b>Presion Atmosferica: </b>".$extraido['presion']." hPa<br>";
+  echo "<b>Humedad ambiental: </b>".$extraido['humedadamb']." %<br>";
+  echo "<b>Humedad del suelo 1: </b>".$extraido['humedadsuelo1']." %<br>";
+  echo "<b>Humedad del suelo 2: </b>".$extraido['humedadsuelo2']." %<br>";
+  echo "<b>Humedad del suelo 3: </b>".$extraido['humedadsuelo3']." %<br>";
+  echo "<b>Iluminancia: </b>".$extraido['luz']." lux<br>";
+  echo "<b>Punto de rocio: </b>".$extraido['procio']." grados Celsius<br>";
+  echo "<b>Velocidad del viento: </b>".$extraido['velocidadviento']." km/h<br>";
+  echo "<b>Sensacion termica: </b>".$extraido['stermica']." grados Celsius<br>";
+  echo "<br>";
+  echo "<hr>";
+} 
+
+mysqli_close($conexion);
+?>
+
+</h4>
+
+
+
+
+</body>
+</html>
---
+++ web/nuevoactual.php
@@ -0,0 +1,78 @@
+<html><head>
+    <title>Estacion Meteorologica - Rivadavia, Mendoza</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="index.css" rel="stylesheet" type="text/css">
+  </head><body>
+    <div class="section section-primary">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h2 class="text-center">Estación meteorológica
+              <br>Visualizacion de datos en tiempo real</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="container">
+	  <IMG SRC="/gifclima.gif">
+
+        <h4 class="text-center">
+            <?php 
+			
+			$conexion=mysqli_connect( "mysql.hostinger.com.ar",
+            "db_user", "db_pass", "db_name") or die( "Problemas con la conexión");
+            $numero=mysqli_query($conexion, "SELECT * FROM estacion ORDER BY numero DESC LIMIT 0 , 1") 
+			or die( "Problemas en el select:".mysqli_error($conexion)); 
+			//$extraido=mysqli_fetch_array($numero); 
+			
+			/* $maximo=$ extraido[ 'numero']; $datos=mysqli_query($conexion,
+            "SELECT * FROM  `estacion` WHERE  `numero` ="$maximo " LIMIT 0 , 30") or die( "Problemas en el select:".mysqli_error($conexion));
+            $informacion=m ysqli_fetch_array($fecha); echo $informacion[ 'fecha'] */
+			
+			
+            while ($extraido=mysqli_fetch_array($numero)) { 
+			
+			echo "<b>Fecha y hora (UTC): </b>".$extraido[ 'fecha']. "<br>"; 
+			echo "<b>Numero de registro: </b>".$extraido['numero']. "<br>"; 
+			echo "<b>Ubicación de estación: </b>".$extraido[ 'id']."<br>"; 
+			echo "<b>Temperatura LM 35: </b>".$extraido[ 'templm']." grados Celsius<br>"; 
+			echo "<b>Temperatura DHT 11: </b>".$extraido[ 'tempdht']." grados Celsius<br>"; 
+			echo "<b>Temperatura BMP 180: </b>".$extraido[ 'tempbmp']." grados Celsius<br>"; 
+			echo "<b>Temperatura PROMEDIO: </b>".$extraido[ 'tempprom']." grados Celsius<br>"; 
+			echo "<b>Presion Atmosferica: </b>".$extraido[ 'presion']." hPa<br>"; 
+			echo "<b>Humedad ambiental: </b>".$extraido[ 'humedadamb']. " %<br>";
+            echo "<b>Humedad del suelo 1: </b>".$extraido[ 'humedadsuelo1']." %<br>"; 
+			echo "<b>Humedad del suelo 2: </b>".$extraido[ 'humedadsuelo2']." %<br>"; 
+			echo "<b>Humedad del suelo 3: </b>".$extraido[ 'humedadsuelo3']." %<br>"; 
+			echo "<b>Iluminancia: </b>".$extraido[ 'luz']. " lux<br>"; 
+			echo"<b>Punto de rocio: </b>".$extraido[ 'procio']. " grados Celsius<br>"; 
+			echo"<b>Velocidad del viento: </b>".$extraido[ 'velocidadviento']. " km/h<br>"; 
+			echo"<b>Sensacion termica: </b>".$extraido[ 'stermica']. " grados Celsius"; 
+			} 
+			mysqli_close($conexion); 
+			?>
+        </h4>
+      </div>
+    </div>
+    <div class="section"></div>
+    <footer class="section section-primary">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+            <h1></h1>
+            <p>Matías Caparros - 2016 - UTN FRM
+              <br>Ubicación de la estación meteorológica: Rivadavia, Mendoza, Argentina
+              <br>Latitud: -33.162774 | Longitud: -68.44462
+              <br>caparrosmatias96@gmail.com</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  
+
+</body></html>
